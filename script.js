36f25b6fe0e6961336c2346fc31c0edc79a01202
Akshita171004 <akshitakaushik17@gmail.com>
2025-05-12 13:24:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,8 +8,22 @@ function convertToRoman(num) {
       5:['V', 5], 
       6:['I', 1]
     };
-
-  //your code here
+let n=798;
+	let ans=""
+	while(n!=0)
+		{
+			for(let i in obj)
+				{
+					let value=obj[i][0];
+					let num=obj[i][1]
+					if (num<=n)
+					{
+						ans=ans+value;
+						n=n-num;
+						break;
+					}
+				}
+		}
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
