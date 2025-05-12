140637db34a8f9fe5263f3f61cc107a251cf68cd
Akshita171004 <akshitakaushik17@gmail.com>
2025-05-12 14:10:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
 const romanMap = [
-  ['M', 1000],
-  ['CM', 900],
-  ['D', 500],
-  ['CD', 400],
-  ['C', 100],
-  ['XC', 90],
-  ['L', 50],
-  ['XL', 40],
-  ['X', 10],
-  ['IX', 9],
-  ['V', 5],
-  ['IV', 4],
-  ['I', 1]
+  0:['M', 1000],
+  1:['CM', 900],
+  2:['D', 500],
+  3:['CD', 400],
+  4:['C', 100],
+  5:['XC', 90],
+  6:['L', 50],
+  7:['XL', 40],
+  8:['X', 10],
+  9:['IX', 9],
+  10:['V', 5],
+  11:['IV', 4],
+  12:['I', 1]
 ];
 
-let n = 798;
 let ans = "";
-
-for (let [roman, value] of romanMap) {
-  while (n >= value) {
-    ans += roman;
-    n -= value;
-  }
+while (num !== 0){
+	for(let i in roadmap){
+		let[symbol,value] = romanMap[i];
+		if(value <= num)
+			ans += symbol;
+			num -= value;
+		break;
+	}
 }
-
-console.log(ans);
